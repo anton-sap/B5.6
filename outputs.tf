aef21fc36e4b6af59cf800f9075f0989acd4cbaf
Anton Saprikin <saprikin.anton@gmail.com>
2023-01-14 00:02:10 +0500

    - Minor changes in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "ya-vm-2_internal_ip_address" {
 output "ya-vm-2_external_ip_address" {
   value = module.ya-vm-2.external_ip_address_vm
 }
+
+output "lb_external_ip" {
+  value = [for s in yandex_lb_network_load_balancer.web-servers.listener: s.external_address_spec.*.address].0[0]
+}
