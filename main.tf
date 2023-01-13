6574e41fa18b46555795fddbf7f64c3cda7d56d3
Anton Saprikin <saprikin.anton@gmail.com>
2023-01-13 17:17:11 +0500

    - Added outputs supoprt - Minor changes in main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = ">= 0.13"
+
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
       version = "0.84.0"
     }
   }
-  required_version = ">= 0.13"
 
   backend "s3" {
     endpoint   = "storage.yandexcloud.net"
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "ya-vm-1_internal_ip_address" {
+  value = module.ya-vm-1.internal_ip_address_vm
+}
+
+output "ya-vm-1_external_ip_address" {
+  value = module.ya-vm-1.external_ip_address_vm
+}
+
+output "ya-vm-2_internal_ip_address" {
+  value = module.ya-vm-2.internal_ip_address_vm
+}
+
+output "ya-vm-2_external_ip_address" {
+  value = module.ya-vm-2.external_ip_address_vm
+}
